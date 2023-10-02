d9cf6849fe293646e42300b1d77798a82606dea8
Gopal Meena <meenagopal24@gmail.com>
2023-10-02 23:39:18 +0530

    worked on : otp sending functionality

---
+++ backend/src/utils/Constants.js
@@ -0,0 +1,7 @@
+const API_URL = 'https://api.textlocal.in/send/?'
+const HEADER = 'GLMEEN'
+const OTP_TEMPLATE = 'is your OTP code PLEASE DO NOT SHARE WITH ANYONE From Gopal Meena'
+
+
+
+module.exports = { API_URL, HEADER, OTP_TEMPLATE }

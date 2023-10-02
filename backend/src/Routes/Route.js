8f0b9af855df7f1d0692b81143e2422bf6da4340
ayushasati <asati1687@gmail.com>
2023-10-02 16:44:21 +0530

    modified_logincontroller

--- backend/src/Routes/Route.js
+++ backend/src/Routes/Route.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const otpController = require('../controllers/otpController');
 
 //login controller 
 const login = require('../controllers/loginController')
 
 
-router.post("/loginSendOtp", login.generateOtp)
-router.post("/sendOtp", otpController.sendOtp);
-
+router.post("/loginSendOtp", login.generateOtp);
 
 module.exports = router;
--- backend/src/controllers/loginController.js
+++ backend/src/controllers/loginController.js
@@ -1,4 +1,5 @@
 //otp Schema for database
+const { text } = require('body-parser');
 const otpSchema = require('../Models/otpSchema')
 // static class for functions
 const functions = require('../utils/functions')
@@ -6,3 +7,36 @@ exports.generateOtp = async (request, response) => {
     const mOtp = functions.generateOTP();
     response.send("otp genrated " + mOtp)
 }
+
+exports.sendOTP = async (request, response) => {
+    const { mobile } = request.body;
+    const otpCode = functions.generateOTP(); 
+
+    try {
+        const existingOtpData = await otpSchema.findOne({ mobile });
+
+        if (existingOtpData) {
+            console.log('Daily User');
+        } else {
+           
+            console.log('New User');
+
+            const newOtpData = new otpSchema({
+                mobile: mobile,
+                code: otpCode,
+                _id : idText,
+                lastToken:tokenToString,
+            });
+
+            await newOtpData.save();
+        }
+
+       // code for process send SMS
+
+        // Respond to the client
+        response.json({ message: "OTP sent successfully" });
+    } catch (error) {
+        // Handle errors, e.g., database errors or SMS sending failures
+        response.status(500).json({ error: "Internal server error" });
+    }
+};
